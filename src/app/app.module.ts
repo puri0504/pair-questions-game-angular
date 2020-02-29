6a85c8f358ff9eb5f689c41fb4bd91ac4d5f264a
Ksenia Uzlova <kseniya.uzlova@simbirsoft.com>
2020-02-29 22:11:12 +0300

    removed answer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { QuestionComponent } from './question/question.component';
-import { AnswerComponent } from './answer/answer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionsComponent,
-    QuestionComponent,
-    AnswerComponent
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
