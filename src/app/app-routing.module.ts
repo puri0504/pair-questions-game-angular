0dcde2130ad72d4421fbc09c2d4d1dafa6dbfd71
Ksenia Uzlova <kseniia.uzlova@fsight.ru>
2020-07-27 13:10:28 +0300

    added app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from "./app.component";
+import { AuthComponent } from "./components/auth/auth.component";
+import { QuestionsComponent } from "./components/questions/questions.component";
+import { AnswersComponent } from "./components/answers/answers.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+  {
+    path: 'questions',
+    component: QuestionsComponent
+  },
+  {
+    path: 'answers',
+    component: AnswersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
